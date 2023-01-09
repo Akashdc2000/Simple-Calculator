b7255b29593353b8f793f92477e99fb4d9ace5ad
Akash <akashchaudhari0002@gmail.com>
2023-01-09 23:10:38 +0530

    Final Modification

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
 let string = "";
-var ud;
+
 let buttons = document.querySelectorAll('.button');
+
+
 function setString() {
-  string = document.getElementById('input').value;
-  console.log(string);
+  string += document.getElementById('input').value;
+  // console.log(string);
 }
-console.log(string);
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      try{
-          string = eval(string);
-          if(string==ud)
-            document.querySelector('input').value = '0';
-          else
-            document.querySelector('input').value = string;
+// console.log(string);
+
+
+Array.from(buttons).forEach((button) => {
+
+  button.addEventListener('click', (e) => {
+
+    if (e.target.innerHTML == '=') {
+      try {
+        string = eval(string);
+        if (string == undefined)
+          document.querySelector('input').value = '0';
+        else
+          document.querySelector('input').value = string;
       }
-      catch(error)
-      {
-        string="Syntax Error";
+      catch (error) {
+        string = "Syntax Error";
         document.querySelector('input').value = string;
       }
-      
+
     }
-    else if(e.target.innerHTML == 'C'){
+
+    else if (e.target.innerHTML == 'C') {
       string = ""
       document.querySelector('input').value = string;
     }
-    else if(e.target.innerHTML == '%'){
+
+    else if (e.target.innerHTML == '%') {
       string = string + e.target.innerHTML;
       size = string.length;
-      n = Number(string.substring(0, size-1));
-      document.querySelector('input').value = n/100;
+      n = Number(string.substring(0, size - 1));
+      document.querySelector('input').value = n / 100;
     }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
+
+    else {
+      // console.log(e.target.innerHTML)
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+
   })
 })

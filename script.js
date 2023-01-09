0acc1d82ec44625a2efede6e6151b4ad5e2256f0
Akash <akashchaudhari0002@gmail.com>
2023-01-09 12:49:33 +0530

    After Input Validation

--- script.js
+++ script.js
@@ -9,11 +9,19 @@ console.log(string);
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
-      string = eval(string);
-      if(string==ud)
-        document.querySelector('input').value = '0';
-      else
+      try{
+          string = eval(string);
+          if(string==ud)
+            document.querySelector('input').value = '0';
+          else
+            document.querySelector('input').value = string;
+      }
+      catch(error)
+      {
+        string="Syntax Error";
         document.querySelector('input').value = string;
+      }
+      
     }
     else if(e.target.innerHTML == 'C'){
       string = ""
